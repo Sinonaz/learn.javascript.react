fccb9159c816678af0abb204fa71a4abd9f59848
Isaac <53692366+Sinonaz@users.noreply.github.com>
2024-09-21 17:13:58 +0300

    start

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,8 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createElement } from "react"
+import { createRoot } from "react-dom/client"
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const rootElement = document.getElementById("root")
+
+const root = createRoot(rootElement)
+
+root.render(createElement(`div`, {}, `Hello world!`))
