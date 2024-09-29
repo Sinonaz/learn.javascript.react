5289cc85d48db4ff58fbe660acd14cbfaca5a655
Isaac <53692366+Sinonaz@users.noreply.github.com>
2024-09-29 21:59:31 +0300

    ProgressBar component done

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,9 +1,12 @@
 import { Header } from "../Header/Header"
 import { Footer } from "../Footer/Footer"
+import { ProgressBar } from "../ProgressBar/ProgressBar"
 
 export const Layout = ({ children }) => {
 	return (
 		<>
+			<ProgressBar />
+
 			<Header />
 
 			{children}
